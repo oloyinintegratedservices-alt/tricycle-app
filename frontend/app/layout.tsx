1d4d21c69fbbeda413fd1ffb978ce955ea405b4f
abdulganiyy <lasisibalogunopeoluwa@ymail.com>
2026-05-09 12:26:04 +0100

    adding payments and payouts tracking

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Provider from "@/components/Provider";
 import { Toaster } from "@/components/ui/sonner";
+import { Suspense } from "react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,7 +31,9 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <Provider>{children}</Provider>
+        <Provider>
+          <Suspense>{children}</Suspense>
+        </Provider>
         <Toaster />
       </body>
     </html>
